56ce1baaa1bbe1a9818dce5aa97094530801d286
Ricardo Santos <rick.giometti@hotmail.com>
2019-09-30 11:59:31 -0300

    Initial Commit

---
+++ KWIC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KWIC
+//
+//  Created by Ricardo de Castro Giometti Santos on 30/09/19.
+//  Copyright © 2019 Ricardo de Castro Giometti Santos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
