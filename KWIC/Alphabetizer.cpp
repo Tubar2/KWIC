1816cc21140e04d44275ca9c2c7966631aa0370b
Ricardo Santos <rick.giometti@hotmail.com>
2019-10-01 11:58:34 -0300

    Implement removeStops

--- KWIC/Alphabetizer.cpp
+++ KWIC/Alphabetizer.cpp
@@ -7,3 +7,35 @@
 //
 
 #include "Alphabetizer.hpp"
+
+Alphabetizer::Alphabetizer(LineStorage * newdata)
+:data(newdata)
+{
+    cout << "Alphabetizer created" << endl;
+}
+
+void Alphabetizer::removeStops(){
+    
+    string firstWord {};
+    bool change = false;
+    auto dataIT = data->shiftedVariations.begin();
+    auto stopsIT = data->stopWords.begin();
+    
+    while (dataIT != data->shiftedVariations.end()) {
+        firstWord = data->getWordFromString(0, *dataIT);
+        while (stopsIT != data->stopWords.end()) {
+            if (*stopsIT == firstWord) {
+                dataIT = data->shiftedVariations.erase(dataIT);
+                change = true;
+                break;
+            }
+            ++stopsIT;
+        }
+        stopsIT = data->stopWords.begin();
+        if (!change) {
+            ++dataIT;
+        } else{
+            change = false;
+        }
+    }
+}
