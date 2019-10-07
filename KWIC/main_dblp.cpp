bfc710b28040c9025ef2a0a88abc6062b3756c19
Gian Lopes <gianlucas1020@hotmail.com>
2019-10-06 21:14:27 -0300

    Comment to avoid warnigs

--- KWIC/main_dblp.cpp
+++ KWIC/main_dblp.cpp
@@ -1,93 +1,93 @@
-#include <iostream>
+// #include <iostream>
 
-#include "DBLP_Input.hpp"
-#include "Text_Input.hpp"
-#include "Text_Output.hpp"
-#include "CircularShifter.hpp"
-#include "Alphabetizer.hpp"
+// #include "DBLP_Input.hpp"
+// #include "Text_Input.hpp"
+// #include "Text_Output.hpp"
+// #include "CircularShifter.hpp"
+// #include "Alphabetizer.hpp"
 
-bool clearPrevEnv2(string filepath);
+// bool clearPrevEnv2(string filepath);
 
-int main2()
-{
-    string filepath {"Exits/"}, line{};
-    cout << "Enter output file name: (no txt needed)" << endl;
-    getline(cin, line);
-    //line = "saida";
+// int main2()
+// {
+//     string filepath {"Exits/"}, line{};
+//     cout << "Enter output file name: (no txt needed)" << endl;
+//     getline(cin, line);
+//     //line = "saida";
 
-    filepath += line + ".txt";
+//     filepath += line + ".txt";
     
-    //TODO: Solution for clearPrevEnv
-    if (!clearPrevEnv2(filepath)){
-        exit(3);
-    }
+//     //TODO: Solution for clearPrevEnv
+//     if (!clearPrevEnv2(filepath)){
+//         exit(3);
+//     }
     
-    //Definig both types of enums
-    type stops = typeStops;
-    type inputs = typeInput;
+//     //Definig both types of enums
+//     type stops = typeStops;
+//     type inputs = typeInput;
 
-    LineStorage data;
+//     LineStorage data;
     
-    DBLP_Input input{"teste", data, inputs};
-    Text_Input words("Resources/nltkstops.txt", data, stops);
+//     DBLP_Input input{"teste", data, inputs};
+//     Text_Input words("Resources/nltkstops.txt", data, stops);
 
-    input.setup();
-    words.setup();
+//     input.setup();
+//     words.setup();
 
-    //Extracting stop Words
-    words.extract();
+//     //Extracting stop Words
+//     words.extract();
 
-    //Extracting text
-    do {
-        data.deletePrevInfo();
-        input.extract(); //One line extraction
+//     //Extracting text
+//     do {
+//         data.deletePrevInfo();
+//         input.extract(); //One line extraction
         
-        //Creating CircularShifter object pointer
-        CircularShifter * cs = new CircularShifter(data.originalLine);
+//         //Creating CircularShifter object pointer
+//         CircularShifter * cs = new CircularShifter(data.originalLine);
         
-        //Creating all shifts for stored line
-        data.shiftedVariations = cs->makeCircularShifts();
-        cout << "Shifts made: " << data.storedLines() << endl;
+//         //Creating all shifts for stored line
+//         data.shiftedVariations = cs->makeCircularShifts();
+//         cout << "Shifts made: " << data.storedLines() << endl;
         
-        //Creating alphabetizer object pointer
-        Alphabetizer * alph = new Alphabetizer(data);
+//         //Creating alphabetizer object pointer
+//         Alphabetizer * alph = new Alphabetizer(data);
         
-        //Removing stops and alphabetizing
-        alph->removeStops();
-        alph->alphabetiseData();
+//         //Removing stops and alphabetizing
+//         alph->removeStops();
+//         alph->alphabetiseData();
         
-        //Creating output object
-        Text_Output to(filepath, data.shiftedVariations);
+//         //Creating output object
+//         Text_Output to(filepath, data.shiftedVariations);
         
-        to.createFile();
-        to.printOutput();
+//         to.createFile();
+//         to.printOutput();
         
-        //Closing files
-        to.closeFile();
+//         //Closing files
+//         to.closeFile();
         
-        //Deleting pointers
-        delete cs;
-        delete alph;
+//         //Deleting pointers
+//         delete cs;
+//         delete alph;
         
-    } while (!input.reachedEND());
+//     } while (!input.reachedEND());
     
-    //MARK: Perguntas pro prof
-        // - Objeto criado em um do while sai do escopo nas seguintes iterações quando criado com outros parâmetros?
+//     //MARK: Perguntas pro prof
+//         // - Objeto criado em um do while sai do escopo nas seguintes iterações quando criado com outros parâmetros?
     
-    //Closing others
+//     //Closing others
 
-    words.closeFile();
+//     words.closeFile();
 
-    return 0;
-}
+//     return 0;
+// }
 
-bool clearPrevEnv2(string filepath){
+// bool clearPrevEnv2(string filepath){
     
-    bool opened {};
-    ifstream a{};
+//     bool opened {};
+//     ifstream a{};
     
-    a.open(filepath, std::ifstream::out | std::ifstream::trunc);
-    opened = a.is_open();
-    a.close();
-    return opened;
-}
+//     a.open(filepath, std::ifstream::out | std::ifstream::trunc);
+//     opened = a.is_open();
+//     a.close();
+//     return opened;
+// }
