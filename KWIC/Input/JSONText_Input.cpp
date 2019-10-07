cac74099585520a2ca286504ef2adb4a1a7bd541
Gian Lopes <gianlucas1020@hotmail.com>
2019-10-06 23:36:34 -0300

    Add support to read stops from json file

---
+++ KWIC/Input/JSONText_Input.cpp
@@ -0,0 +1,55 @@
+//
+//  DBLP_Input.cpp
+//  KWIC
+//
+//  Created by Gianlucas dos Santos on 06/10/19.
+//  Copyright © 2019 Gianlucas dos Santos. All rights reserved.
+//
+
+#include "JSONText_Input.hpp"
+
+//MARK: Constructor
+JSON_Input::JSON_Input(string query, LineStorage & data, type entryType)
+:Input(query, data, entryType)
+{
+    cout << "Created JSON_Input object for reading." << endl;
+}
+
+bool JSON_Input::setup()
+{
+    cout << "Setting up JSON Input enviroment." << endl;
+    
+    in_file.open(filepath);
+    
+    return in_file.is_open();
+}
+
+void JSON_Input::extractStops(){
+    string line {};
+    std::stringstream sstr;
+    sstr << in_file.rdbuf();
+    json j = json::parse(sstr.str());
+    json o = j["stops"];
+    for (auto it = o.begin(); it != o.end(); ++it)
+    {
+        data.stopWords.push_back(*it);
+    }
+}
+
+//MARK: reachedEND
+bool JSON_Input::reachedEND(){ //Check if eof was reached
+    return in_file.eof();
+}
+
+//TODO
+void JSON_Input::extractMain()
+{
+    std::cout << "Can't read titles from JSON file due to different possibilities of formats,"
+        << "use DBLP_Input module" << std::endl;
+    exit(17);
+}
+
+//MARK: finish
+void JSON_Input::finish(){//Closes file
+    in_file.close();
+}
---
+++ KWIC/Input/JSONText_Input.hpp
@@ -0,0 +1,54 @@
+//
+//  JSON_Input.hpp
+//  KWIC
+//
+//  Created by Gianlucas dos Santos on 06/10/19.
+//  Copyright © 2019 Gianlucas dos Santos. All rights reserved.
+//
+
+#ifndef JSON_INPUT_H
+#define JSON_INPUT_H
+
+//This class reads stop words from a JSON file in this specific format "{"stops" : ["word1", "word2", ... ]}"
+
+#include "I_Input.hpp"
+#include "Input.hpp"
+#include "../LineStorage.hpp"
+
+#include <iostream>
+#include <memory>
+#include "../single_include/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+class JSON_Input : public Input{
+public:
+    ifstream in_file {};
+
+    //MARK: Constructor
+    JSON_Input(string filepath, LineStorage & data, type entryType);
+
+    //MARK: Setup
+    bool setup() override;
+
+    void extractStops() override; //Extracts all stop words in a vector<string>
+
+    // Cant be used
+    void extractMain() override; 
+
+    // return true when all titles in all_titles_vector have been accessed
+    bool reachedEND() override;
+
+    //MARK: finish
+    void finish() override;//Closes file
+
+    //MARK: Destructor
+    virtual ~JSON_Input() = default;
+
+private:
+    std::vector<std::string> all_titles_vector;
+    int title_iterator;
+    int title_count;
+
+};
+#endif
