a9c3ab40ca258c05d2aa6daf90369aa6106b45c3
Ricardo Santos <rick.giometti@hotmail.com>
2019-10-07 22:23:59 -0300

    Implement Gtests

--- KWIC/main_gtest.cpp
+++ KWIC/main_gtest.cpp
@@ -3,29 +3,30 @@
 
 #include <gtest/gtest.h>
 
-class LineStorageTest : public testing::Test{
+class EmptyLineStorageTest : public testing::Test{
 protected:
-    LineStorage *data;
-    
-    void SetUp() override{
-        data = new LineStorage;
-    }
-    void TearDown() override{
-        delete data;
-    }
+    LineStorage data;
 };
 
-TEST_F(_LineStorage, emptyCreation){
-    ///Testing empty generation for empty LineStorage
-    ASSERT_EQ(data->originalLine_String, "");
-    ASSERT_EQ(data->originalLine_Vector, "");
-    ASSERT_EQ(data->shiftedVariations, "");
-    ASSERT_EQ(data->stopWords, "");
-
-    ///Testing functions on empty LineStorage
-    ASSERT_EQ(data.charsInWord, 0);
+TEST_F(EmptyLineStorageTest, empty_original_line_string){
+    ASSERT_TRUE(data.originalLine_String.empty());
 }
 
+TEST_F(EmptyLineStorageTest, empty_original_line_vector){
+    ASSERT_TRUE(data.originalLine_Vector.empty());
+}
+
+TEST_F(EmptyLineStorageTest, empty_shifted_variations){
+    ASSERT_TRUE(data.shiftedVariations.empty());
+}
+
+TEST_F(EmptyLineStorageTest, empty_stop_words){
+    ASSERT_TRUE(data.stopWords.empty());
+}
+
+TEST_F(EmptyLineStorageTest, methodCall_charsInWord_EQ_0){
+    ASSERT_EQ(data.wordsInLine(), 0);
+}
 
 int main(int argc, char **argv){
 
