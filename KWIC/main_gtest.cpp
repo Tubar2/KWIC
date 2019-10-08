e4478e261a0a7aff69e0265eede5ea667a692be0
Ricardo Santos <rick.giometti@hotmail.com>
2019-10-08 12:11:23 -0300

    Update main ~> tests all tests

--- KWIC/main_gtest.cpp
+++ KWIC/main_gtest.cpp
@@ -1,9 +1,24 @@
-// #include "Unit_Tests/LineStorageTest.cpp"
-// #include "Unit_Tests/AlphabetizerTest.cpp"
-
+#include "Unit_Tests/LineStorageTest.cpp"
+#include "Unit_Tests/AlphabetizerTest.cpp"
+#include "Unit_Tests/CircularShifterTest.cpp"
 
 #include <iostream>
 #include <gtest/gtest.h>
+
+#include "Input/DBLP_Input.hpp"
+#include "LineStorage.hpp"
+
+class DBLP_Input_Test : public testing::Test{
+protected:
+    LineStorage data;
+    type stops = typeStops;
+    type in = typeInput;
+    DBLP_Input input();
+
+    void SetUp() override{
+        
+    }
+};
 
 int main(int argc, char **argv){
 
