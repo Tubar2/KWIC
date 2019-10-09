76e0dfafc188ba6c5f92dc69223f00d1efe580e8
Ricardo Santos <rick.giometti@hotmail.com>
2019-10-09 08:04:57 -0300

    Implement tests on DBLP_Input

--- KWIC/Unit_Tests/DBLP_InputTest.cpp
+++ KWIC/Unit_Tests/DBLP_InputTest.cpp
@@ -4,23 +4,53 @@
 #include "../LineStorage.hpp"
 #include "../Input/DBLP_Input.hpp"
 
-class DBLP_Input_Test : public testing::Test{
+class withQuery_DBLP_Input_Test : public testing::Test{
 protected:
     LineStorage data;
 
     type stops = typeStops;
     type inputs = typeInput;
 
-    DBLP_Input myinput{data, inputs};
-
-    void SetUp() override{
-        
-    }
-    void TearDown() override{
-
-    }
+    DBLP_Input myinput{"Parnas", data, inputs};
 };
 
-TEST_F(DBLP_Input_Test, input){
-    
+class noQuery_DBLP_Input_Test : public testing::Test{
+protected:
+    LineStorage data;
+
+    type stops = typeStops;
+    type inputs = typeInput;
+
+    DBLP_Input myinput{"", data, inputs};
+};
+
+TEST_F(withQuery_DBLP_Input_Test, methodCall_setup_filledQuery){
+    //Assert
+    ASSERT_TRUE(myinput.setup());
 }
+
+TEST_F(withQuery_DBLP_Input_Test, methodCall_extract_filledQuery){
+    //Solution
+    std::vector<std::string> solution {"Parnas", "on", "Parnas", "-", "a", "life", "of", "indecision."};
+    
+    //Act
+    myinput.open();
+    myinput.extract();
+
+    //Assert
+    ASSERT_EQ(data.originalLine_Vector, solution);
+}
+
+TEST_F(noQuery_DBLP_Input_Test, methodCall_setup_noQuery){
+    //Assert
+    ASSERT_FALSE(myinput.setup());
+}
+
+TEST_F(noQuery_DBLP_Input_Test, methodCall_extract_noQuery){
+    //Assert
+    myinput.open();
+    myinput.extract();
+
+    ASSERT_TRUE(data.originalLine_Vector.empty());
+}
+
