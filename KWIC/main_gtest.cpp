312d6f99571c1e61db224f638e748be0cfa36c98
Ricardo Santos <rick.giometti@hotmail.com>
2019-10-09 08:03:50 -0300

    main now compiles with make

--- KWIC/main_gtest.cpp
+++ KWIC/main_gtest.cpp
@@ -1,24 +1,13 @@
 #include "Unit_Tests/LineStorageTest.cpp"
 #include "Unit_Tests/AlphabetizerTest.cpp"
 #include "Unit_Tests/CircularShifterTest.cpp"
+#include "Unit_Tests/DBLP_InputTest.cpp"
 
 #include <iostream>
 #include <gtest/gtest.h>
 
 #include "Input/DBLP_Input.hpp"
 #include "LineStorage.hpp"
-
-class DBLP_Input_Test : public testing::Test{
-protected:
-    LineStorage data;
-    type stops = typeStops;
-    type in = typeInput;
-    DBLP_Input input();
-
-    void SetUp() override{
-        
-    }
-};
 
 int main(int argc, char **argv){
 
